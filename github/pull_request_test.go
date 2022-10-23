84d30556b11efb2c50979ac56b29f355e993b0f8
Eli Block <3347571+eliblock@users.noreply.github.com>
2022-10-22 18:39:56 -0700

    tests for file loading 🧪

--- github/pull_request_test.go
+++ github/pull_request_test.go
@@ -2,7 +2,10 @@ package github
 
 import (
 	"fmt"
+	"reflect"
 	"testing"
+
+	"github.com/google/go-github/v47/github"
 )
 
 func (bound lineBound) String() string {
@@ -19,6 +22,129 @@ func lineBoundsString(bounds []lineBound) string {
 		combined = fmt.Sprintf("%s, %s", combined, bound)
 	}
 	return combined
+}
+
+func (file *pullRequestFile) String() string {
+	if file == nil {
+		return "<nil file>"
+	}
+	return fmt.Sprintf("%s with patches on %s", file.filename, lineBoundsString(file.lineBounds))
+}
+
+func pullRequestFilesString(files []*pullRequestFile) string {
+	combined := "<none>"
+	for i, file := range files {
+		if i == 0 {
+			combined = file.String()
+			continue
+		}
+		combined = fmt.Sprintf("%s, %s", combined, file)
+	}
+	return combined
+}
+
+func TestSdkFilesToInternalFiles(t *testing.T) {
+	filename_1 := "src/main.go"
+	patch_1 := `@@ -0,0 +1,2 @@
++1
++2`
+	bounds_1, _ := patchToLineBounds(patch_1)
+
+	filename_2 := "src/main_2.go"
+	patch_2 := `@@ -3,2 +3,2 @@ hi there
+-1
++1
++2`
+	bounds_2, _ := patchToLineBounds(patch_2)
+
+	github_file_no_patch := github.CommitFile{
+		Filename: &filename_1,
+	}
+	github_file_no_name := github.CommitFile{
+		Patch: &patch_1,
+	}
+
+	github_file_1 := github.CommitFile{
+		Filename: &filename_1,
+		Patch:    &patch_1,
+	}
+	github_file_2 := github.CommitFile{
+		Filename: &filename_2,
+		Patch:    &patch_2,
+	}
+
+	internal_file_1 := pullRequestFile{
+		filename:   filename_1,
+		patch:      patch_1,
+		lineBounds: bounds_1,
+	}
+	internal_file_2 := pullRequestFile{
+		filename:   filename_2,
+		patch:      patch_2,
+		lineBounds: bounds_2,
+	}
+
+	var tests = []struct {
+		name          string
+		sdkFiles      []*github.CommitFile
+		internalFiles []*pullRequestFile
+	}{
+		{
+			"nil file",
+			[]*github.CommitFile{nil},
+			[]*pullRequestFile{},
+		},
+		{
+			"one file",
+			[]*github.CommitFile{&github_file_1},
+			[]*pullRequestFile{&internal_file_1},
+		},
+		{
+			"multiple files",
+			[]*github.CommitFile{&github_file_1, &github_file_2},
+			[]*pullRequestFile{&internal_file_1, &internal_file_2},
+		},
+		{
+			"one file without patch",
+			[]*github.CommitFile{&github_file_no_patch},
+			[]*pullRequestFile{},
+		},
+		{
+			"one file without name",
+			[]*github.CommitFile{&github_file_no_name},
+			[]*pullRequestFile{},
+		},
+		{
+			"four files, two of which are valid",
+			[]*github.CommitFile{&github_file_1, &github_file_2, &github_file_no_name, &github_file_no_patch},
+			[]*pullRequestFile{&internal_file_1, &internal_file_2},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := sdkFilesToInternalFiles(tt.sdkFiles)
+			if err != nil {
+				t.Errorf("expected no error but received %q", err)
+			}
+
+			for _, internalFile := range tt.internalFiles {
+				found := false
+				for _, foundFile := range got {
+					if reflect.DeepEqual(internalFile, foundFile) {
+						found = true
+						break
+					}
+				}
+				if !found {
+					t.Errorf("expected file %s but did not find it: found %s", internalFile, pullRequestFilesString(got))
+				}
+			}
+
+			if len(got) != len(tt.internalFiles) {
+				t.Errorf("expected %d total files but received %d", len(tt.internalFiles), len(got))
+			}
+		})
+	}
 }
 
 func TestPatchToLineBounds(t *testing.T) {
